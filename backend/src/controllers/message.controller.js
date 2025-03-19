dfaa4d0670a46168d3d66e824e6266d181166585
Sahil1459 <sahilgawade712@gmail.com>
2025-03-20 01:55:30 +0530

    add send message functionality

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.models.js";
 import Message from "../models/message.model.js";
+import router from "../routes/auth.routes.js";
+import { protectRoute } from "../middlewares/auth.middlewares.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -28,6 +31,36 @@ export const getMessages = async (req, res) => {
     res.status(200).json(messages);
   } catch (error) {
     console.log("Error in getMessages: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    //todo: ad relatime functionality
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage: ", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
+router.post("/send", protectRoute, sendMessage);
 
 export default router;
